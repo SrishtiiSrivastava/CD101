8ccce3d428447a729b73b6ecef699dafdba5f54d
SrishtiiSrivastava <srishtis@skillup.tech>
2021-05-11 11:56:02 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    r = document.getElementById("rate").value;
-    y = document.getElementById("years").value;
+   var principal = document.getElementById("principal").value;
+   var rate = document.getElementById("rate").value;
+   var years = document.getElementById("years").value;
     result = document.getElementById("result");
     
     
                
-    var interest = (p*r*y)/100;
+    var interest = (principal*rate*years)/100;
 
-
+    var year = new Date().getFullYear()+parseInt(years);
     
     var amt=interest;
     var result=document.getElementById("result");
     result.innerHTML="If you deposit "+p+",<br> at an interest rate of "+r+"%. <br>You will receive an amount of " +amt+",<br>in the year"+year;
+
+    function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
+    }
 
  
